b0a75c5644dac81d7fca01bc7ac17b9197901c07
Patrick <patrick204nqh@gmail.com>
2025-11-13 22:33:13 +0700

    Refactor sumologic-query formula to use system Ruby and simplify installation process

--- Formula/sumologic-query.rb
+++ Formula/sumologic-query.rb
@@ -4,29 +4,43 @@ class SumologicQuery < Formula
   desc 'Lightweight Ruby CLI for querying Sumo Logic logs quickly'
   homepage 'https://github.com/patrick204nqh/sumologic-query'
   url 'https://github.com/patrick204nqh/sumologic-query/archive/refs/tags/v1.1.0.tar.gz'
-  sha256 '4cb6e2f1377da0d917618ac9dc32db0f2c87452c2270878802f6dd1ccc56dd7c' # Will be calculated after first release
+  sha256 '4cb6e2f1377da0d917618ac9dc32db0f2c87452c2270878802f6dd1ccc56dd7c'
   license 'MIT'
 
-  # Use Homebrew Ruby (pre-built bottle, fast install)
-  depends_on 'ruby'
+  # No dependencies! Uses macOS system Ruby (comes with macOS)
+  # This makes installation instant - no building required
 
   def install
-    ENV['GEM_HOME'] = libexec
+    # Install library files
+    libexec.install Dir['lib/*']
 
-    # Build and install gem with system Ruby
-    system 'gem', 'build', 'sumologic-query.gemspec'
-    system 'gem', 'install', '--no-document', "sumologic-query-#{version}.gem"
+    # Create standalone executable that uses system Ruby
+    (bin / 'sumo-query').write <<~EOS
+      #!/usr/bin/env ruby
+      # frozen_string_literal: true
 
-    # Create wrapper script with proper gem paths
-    (bin / 'sumo-query').write_env_script(
-      libexec / 'bin/sumo-query',
-      GEM_HOME: libexec,
-      GEM_PATH: libexec
-    )
+      $LOAD_PATH.unshift('#{libexec}')
+      load '#{libexec}/../bin/sumo-query'
+    EOS
+
+    # Install the actual CLI script
+    libexec.install 'bin/sumo-query'
   end
 
   test do
     # Test that the binary exists and shows version
     assert_match "sumologic-query v#{version}", shell_output("#{bin}/sumo-query --version")
+  end
+
+  def caveats
+    <<~EOS
+      This formula requires Ruby 2.7 or later.
+      macOS Catalina (10.15) and later include Ruby 2.6, so you may need to:
+        - Install Ruby via Homebrew: brew install ruby
+        - Or use a Ruby version manager (rbenv, asdf, etc.)
+
+      To verify your Ruby version:
+        ruby --version
+    EOS
   end
 end
